60738b45ba2a855f57e7c2dd873db523d4c9d416
Lioner <leonardorcruz2000@gmail.com>
2026-03-29 13:53:09 -0300

    upd de progresso

--- scripts/clean.py
+++ scripts/clean.py
@@ -4,12 +4,64 @@ from pathlib import Path
 
 raw = Path("data/raw")
 psd = Path("data/processed")
-
-
 psd.mkdir(exist_ok=True)
-          
 csv_raw = raw.glob("*.csv")
+    
+limpos = []
+errors = []
+mapa_colunas =  {
+  'id' : ["id", "id_compra", "compra_id", "id_pedido", "pedido_id"],
+  'cliente' : ["nome", "nome_cliente", "cliente_nome"],
+  'valor_produto' : ["preço", "preco", "valor_compra", "valor", "preco_unitario"],
+  'quantidade' : ["qtd", "unidades", "quantia"],
+  'cidade' : ["loja", "região", "regiao", "local"]
+}
+obg_column = [
+  'id', 'valor_produto', 'quantidade'
+]
+
+
+def padronizar_colunas(df, mapa):
+    #padronização de texto
+    df.columns = (
+     df.columns
+     .str.strip()
+     .str.lower()
+     .str.replace(' ', '_')
+    )
+
+    #mudança de nomes variaveis em colunas para o padrão do script
+    colunas_novas = {}
+    for col_padrao, variacoes in mapa.items():
+      for col in df.columns:
+        if col in variacoes:
+          colunas_novas[col] = col_padrao
+    df = df.rename(columns=colunas_novas)
+    return df
+
+
+def validar_colunas(df):
+    mssg_clm = [col for col in obg_column if col not in df.columns]
+    if mssg_clm:
+      raise ValueError(f"Colunas Faltando: {mssg_clm}")
+
+
+def limpeza(df):
+   df = df.dropna(subset=obg_column)
+   df = df.drop_duplicates(subset='id', keep='first')
+   df['cliente'] = df['cliente'].fillna("desconhecido")
+   df['cidade'] = df['cidade'].fillna("desconhecido")
+   return df
+
+
+
+
+
 
 for i in csv_raw:
   df = pd.read_csv(i)
-  print(df)
+  df = padronizar_colunas(df, mapa_colunas)
+  df = limpeza(df)
+  print(df.info())
+          
+
